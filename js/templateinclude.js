f55600a784219260aa4ab8b7b851da2f7aa9fa3c
Chrysaour <chrysaour@yahoo.co.jp>
2015-10-29 18:31:18 -0400

    Creating template

--- js/templateinclude.js
+++ js/templateinclude.js
@@ -8,4 +8,17 @@ $(document).ready(function(){
   $.get("template/navbar.html", function(data) {
     $("#navbar").html(data);
   });
-}); 
+});
+
+$(document).ready(function(){
+  $.get("template/bottom.html", function(data) {
+    $("#bottombar").html(data);
+  });
+});
+
+$(document).ready(function(){
+  $.get("template/footer.html", function(data) {
+    $("#footerbar").html(data);
+  });
+});
+
